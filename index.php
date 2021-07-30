1e8c2a52c7a443a31dd4b52bbd129fc2bd4fea54
Nicolas L <lorion06@gmail.com>
2021-07-30 12:16:20 +0200

    initializing html page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width-viewport=width initial-scale=1.0">
+        <title>Image hoster</title>
+    </head>
+<body>
+    <h1>Image Hoster</h1>
+    <section>
+        
+    </section>
+</body>
+</html>
